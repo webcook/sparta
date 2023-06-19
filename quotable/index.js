a7e1ca32cb85e286b3aac83f93cea5cd3e50983f
dongjobs <webcook@teamblind.com>
2023-06-19 14:22:35 +0900

    trans

--- quotable/index.js
+++ quotable/index.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     renderQuote()
     clock();
     setInterval(clock, 1000); // 1초마다 실행
-    
+
 
 });
 
@@ -12,7 +12,7 @@ async function fetchText() {
     //let response = await fetch('https://worldtimeapi.org/api/timezone/Asia/Seoul');
     //let data = await response.text();
     //return data;
-    
+
     let url = 'https://worldtimeapi.org/api/timezone/Asia/Seoul';
     try {
         let res = await fetch(url);
@@ -25,7 +25,7 @@ async function fetchText() {
     } catch (error) {
         console.log(error);
     }
-    
+
 
 
 }
@@ -49,6 +49,7 @@ function renderQuote() {
     let url = `https://api.quotable.io/random`
     fetch(url)
         .then(res => res.json()).then((data) => {
+            console.log(data);
             let content = `" ${data['content']} "`
             let author = `- ${data['author']} -`
             $('#content').text(content)
@@ -63,14 +64,14 @@ function clock() {
     var minutes = time.getMinutes();
     var seconds = time.getSeconds();
     var AmPm ="AM";
-    if(hours > 12){   
+    if(hours > 12){
         var AmPm ="PM";
         hours %= 12;
     }
 
     $('#time').text(`${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds} ${AmPm}`);
 
-    //Target.innerText = 
+    //Target.innerText =
     //`${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
 
     //Target_apm.innerText = `${AmPm}`;
