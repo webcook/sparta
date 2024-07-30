451a90cca0568655252fd31f436a451af04f6448
dongjobs <webcook@teamblind.com>
2024-07-30 15:32:35 +0900

    clean code, random background

--- quotable/index.js
+++ quotable/index.js
@@ -1,80 +1,60 @@
 $(document).ready(function () {
-    fetchText();
-    renderCurrentTime()
-    renderQuote()
-    clock();
-    setInterval(clock, 1000); // 1초마다 실행
-
-
+  fetchText();
+  renderCurrentTime()
+  renderQuote()
+  clock();
+  setInterval(clock, 500); // 0.5초마다 실행
+  randomBackground();
 });
 
 async function fetchText() {
-    //let response = await fetch('https://worldtimeapi.org/api/timezone/Asia/Seoul');
-    //let data = await response.text();
-    //return data;
-
-    let url = 'https://worldtimeapi.org/api/timezone/Asia/Seoul';
-    try {
-        let res = await fetch(url);
-        // console.log(res);
-        //{type: 'cors', url: 'https://worldtimeapi.org/api/timezone/Asia/Seoul', redirected: false, status: 200, ok: true, …}
-
-        return await res.json()
-        //[[PromiseResult]]:Object  datetime: "2022-11-08T11:20:26.201687+09:00"
-
-    } catch (error) {
-        console.log(error);
-    }
-
-
-
+  let url = 'https://worldtimeapi.org/api/timezone/Asia/Seoul';
+  try {
+    let res = await fetch(url);
+    return await res.json()
+  } catch (error) {
+    console.log(error);
+  }
 }
-
-
-
-
 
 //현재 시간
 function renderCurrentTime() {
-    let url = `https://worldtimeapi.org/api/timezone/Asia/Seoul`
-    fetch(url)
-        .then(res => res.json()).then((data) => {
-            // console.log(data['datetime']);
-            let datetime = data['datetime'].substr(11, 5)
-            // $('#time').text(datetime)
-        })
+  let url = `https://worldtimeapi.org/api/timezone/Asia/Seoul`
+  fetch(url)
+    .then(res => res.json()).then((data) => {
+      let datetime = data['datetime'].substr(11, 5)
+    })
 }
+
 // 명언
 function renderQuote() {
-    let url = `https://api.quotable.io/random`
-    fetch(url)
-        .then(res => res.json()).then((data) => {
-            console.log(data);
-            let content = `" ${data['content']} "`
-            let author = `- ${data['author']} -`
-            $('#content').text(content)
-            $('#author').text(author)
-        })
+  let url = `https://api.quotable.io/random`
+  fetch(url)
+    .then(res => res.json()).then((data) => {
+      // console.log(data);
+      let content = `" ${data['content']} "`
+      let author = `- ${data['author']} -`
+      $('#content').text(content)
+      $('#author').text(author)
+    })
 }
 
 function clock() {
-    var time = new Date();
-    console.log(time);
-    var hours = time.getHours();
-    var minutes = time.getMinutes();
-    var seconds = time.getSeconds();
-    var AmPm ="AM";
-    if(hours > 12){
-        var AmPm ="PM";
-        hours %= 12;
-    }
+  const time = new Date();
+  let hours = time.getHours();
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  let AmPm ="AM";
+  if(hours > 12){
+    AmPm ="PM";
+    hours %= 12;
+  }
+  $('#time').text(`${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds} ${AmPm}`);
+}
 
-    $('#time').text(`${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds} ${AmPm}`);
-
-    //Target.innerText =
-    //`${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
-
-    //Target_apm.innerText = `${AmPm}`;
-
+function randomBackground() {
+  const backgrounds = ['bg01', 'bg02', 'bg03', 'bg04', 'bg05', 'bg06', 'bg07', 'bg08'];
+  const randomBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)];
+  $('body').addClass(randomBackground);
 }
 
